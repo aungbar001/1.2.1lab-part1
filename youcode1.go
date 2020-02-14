25a14a494e39c4c4c76b35d69debacb950f7f396
aungbar001 <aungieb0977@gmail.com>
2020-02-14 06:06:47 +0000

    Initial commit

---
+++ youcode1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello World!")
+  fmt.Println("my name is Aungelia Barletta, And my Magjor is Computer Sience ")
+  fmt.Println("this is my first Semester at Butte College.")
+  fmt.Println("one of my favorite pastimes is Art")
+  
+
+}
